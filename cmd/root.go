a86c69203bc744c16a44b0306731ac9feef209ab
James Anderbard <jamesanderbard@gmail.com>
2024-12-25 01:04:47 -0600

    Add in a more meaningful description of the CLI

--- cmd/root.go
+++ cmd/root.go
@@ -15,13 +15,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "AlbertApiCLI",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A CLI tool to run sets of API calls",
+	Long: ``,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
